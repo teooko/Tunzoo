61bce35dbaca640538dc2e8b91a15299949a4027
teooko <teobogdann@yahoo.com>
2024-10-07 13:35:29 +0300

    Play a song on button press

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect} from "react";
+import * as Tone from 'tone';
 
 function App() {
-  const [count, setCount] = useState(0)
+    let audio: Tone.Player;
 
+    useEffect(() => {
+        audio = new Tone.Player("public\\assets\\peak.mp3").toDestination();
+        audio.autostart = false;
+    }, []);
+
+    const startSong = () => {
+        Tone.loaded().then(() => {
+            if (audio) {
+                audio.start();
+            } else {
+                console.error("Audio player not initialized");
+            }
+        });
+    };
+    
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <button onClick={() => startSong()}>start</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
