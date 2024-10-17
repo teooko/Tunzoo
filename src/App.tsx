a46c59d2942c08bf6a3b86b84e3e7ce8760e04a4
teooko <teobogdann@yahoo.com>
2024-10-17 19:34:26 +0300

    Tweak background elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import {Canvas, useLoader} from "@react-three/fiber";
-import { OrbitControls, Box } from "@react-three/drei";
+import {Canvas, useLoader, useThree} from "@react-three/fiber";
+import {OrbitControls, Box, Sphere, CubicBezierLine, Ring, Dodecahedron, Circle} from "@react-three/drei";
 import './App.css';
 import { useEffect, useRef } from "react";
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {TextureLoader} from "three";
 import {motion} from "framer-motion";
@@ -34,25 +32,32 @@ function App() {
             };
         }, [glbUrl]);
 
-        return <group ref={modelRef} />;
+        return <group ref={modelRef} position={[-2, -1, 2]} rotation={[0, Math.PI * 0.3, 0]}/>;
     };
 
     const Plane = () => {
         const texture = useLoader(TextureLoader, 'public/assets/texture.png'); // Use the PNG version of your SVG
         
         return (
-            <mesh position={[0, 5, -10]}>
+            <mesh position={[0, 0, -10]}>
                 <planeGeometry args={[40, 20]} /> {/* Width and Height of the Plane */}
+                <meshStandardMaterial map={texture} color="#00D9FF" transparent={true} opacity={0.3}/>
+            </mesh>
+        );
+    };
+    const Fern = () => {
+        const texture = useLoader(TextureLoader, 'public/assets/fern.png'); // Use the PNG version of your SVG
+
+        return (
+            <mesh position={[-3,-1.8, 2]}>
+                <planeGeometry args={[2, 1]} /> {/* Width and Height of the Plane */}
                 <meshStandardMaterial map={texture} color="#00D9FF" transparent={true} />
             </mesh>
         );
     };
-
     return (
         <>
-            <div style={{position: "absolute", height: "80px", backgroundColor: "black", zIndex: 2}}>
-
-            </div>
+            
                 <motion.div
                     initial={{ backgroundColor: '#66CD79' }} 
                     animate={{ backgroundColor: '#03CEA4' }} 
@@ -64,19 +69,27 @@ function App() {
                     }}
                     style={{height: "100vh", width: "100vw" }}
                 >
-                
-                <Canvas>
+                    <div style={{height: "6%", backgroundColor: "black", zIndex: 2}} />
+                <Canvas style={{
+                    height: "94%"
+                }}>
                     {/* Camera Controls */}
                     <OrbitControls />
 
                     {/* Lighting */}
-                    <ambientLight intensity={10} color={'#66CD79'} /> {/* Soft overall light */}
+                    <ambientLight intensity={5} color={'#66CD79'} /> {/* Soft overall light */}
                     <directionalLight position={[0, 10, 5]} intensity={1} /> {/* Strong directional light */}
                     <pointLight position={[5, 5, 5]} intensity={1} decay={2} distance={10} /> {/* Point light for localized illumination */}
                     <spotLight position={[0, 10, 0]} angle={0.3} penumbra={0.5} intensity={1} /> {/* Spot light for focused beam */}
                     
                     <GltfModel glbUrl="public/assets/GLTF/Inkfish_LOD1.glb" />
                     <Plane />
+                    <Fern />
+                    <Dodecahedron position={[5, 5, -5]}>
+                        <meshStandardMaterial color="white" metalness={0.2} /> 
+                    </Dodecahedron>
+                    <Ring position={[3, 4, -3]}/>
+                    
                 </Canvas>
                 </motion.div>
         </>
