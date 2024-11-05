dc38d4c2f2ab79355a205ee4ed78d8b5495cd7de
teooko <teobogdann@yahoo.com>
2024-11-05 17:50:06 +0200

    Extract caret components

--- src/scenes/CanvasMenu.tsx
+++ src/scenes/CanvasMenu.tsx
@@ -1,12 +1,11 @@
 ﻿import {useState} from 'react';
 import CameraController from "./CameraController.tsx";
-import {Polyhedron} from "@react-three/drei";
 import Model from "./Model.tsx";
 import TexturedPlane from "./TexturedPlane.tsx";
-import Caret from "./Caret.tsx";
 import SpinningModel from "./SpinningModel.tsx";
 import {Canvas} from "@react-three/fiber";
 import useMousePosition from "./useMousePosition.tsx";
+import Carets from "./Carets.tsx";
 
 const CanvasMenu = () => {
     const mousePosition = useMousePosition();
@@ -15,12 +14,10 @@ const CanvasMenu = () => {
     return (
         <Canvas className="relative h-[94%] z-10" >
             <CameraController mousePosition={mousePosition} />
-            <Polyhedron />
-            <ambientLight intensity={5} color={'white'} /> {/* Soft overall light */}
+            <ambientLight intensity={5} color={'white'} />
             <Model position={[-1.6, -1, 3]} rotation={[0, Math.PI * 0.3, 0]} modelIndex={playerIndex}/>
             <TexturedPlane texturePath={'public/assets/texture.png'} position={[0, 0, -5]} size={[30, 15]}/>
-            <Caret texturePath={'public/assets/caretLeft.png'} position={[-2.6, -0.5, 3]} size={[0.1, 0.2]} onClick={() => setPlayerIndex(state => state - 1 < 0 ? 7 : state - 1)}/>
-            <Caret texturePath={'public/assets/caretRight.png'} position={[-0.6, -0.5, 3]} size={[0.1, 0.2]} onClick={() => setPlayerIndex(state => state + 1 > 7 ? 0 : state + 1)}/>
+            <Carets setPlayerIndex={setPlayerIndex} />
             <SpinningModel position={[0, -3, 0]} rotation={[0, 0, 0]} />
             <SpinningModel position={[-12, 4, -3]} rotation={[3, 0, 0]} />
             <SpinningModel position={[-6, 3, -3]} rotation={[0, 0, 0]} />
---
+++ src/scenes/Carets.tsx
@@ -0,0 +1,18 @@
+﻿import Caret from "./Caret.tsx";
+
+const Carets = ({setPlayerIndex}) => {
+    const handlePressLeft = () => {
+        setPlayerIndex((state: number) => state - 1 < 0 ? 7 : state - 1);
+    }
+    const handlePressRight = () => {
+        setPlayerIndex((state: number) => state + 1 > 7 ? 0 : state + 1)
+    }
+    return (
+        <>
+            <Caret texturePath={'public/assets/caretLeft.png'} position={[-2.6, -0.5, 3]} size={[0.1, 0.2]} onClick={handlePressLeft}/>
+            <Caret texturePath={'public/assets/caretRight.png'} position={[-0.6, -0.5, 3]} size={[0.1, 0.2]} onClick={handlePressRight}/>
+        </>
+    );
+};
+
+export default Carets;
