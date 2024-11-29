4c4972bb5e225e5ede6f09afdedbfe89404a301b
teooko <teobogdann@yahoo.com>
2024-11-29 17:40:41 +0200

    Extract hit reference

---
+++ src/gameLogic/HitReference.tsx
@@ -0,0 +1,32 @@
+﻿import React from 'react';
+import HitQualityAnimation from "./HitQualityAnimation.tsx";
+import {motion} from "framer-motion";
+import {HitQuality} from "../../lib/types.ts";
+import {useAnimationStore} from "./Stores/animationStore.tsx";
+import {useScoringStore} from "./Stores/scoringStore.tsx";
+
+const HitReference = () => {
+    const {shadow, jump, disableShadow, disableJump, toggleJumpRight } = useAnimationStore((state) => state);
+    const {hitQuality, score, combo} = useScoringStore(state => state);
+    const addShadow = (hitQuality) => {
+        if(shadow && hitQuality === HitQuality.Perfect)
+            return "0px 0px 50px 10px #50DD49";
+        else if(shadow && hitQuality === HitQuality.Good)
+            return "0px 0px 50px 10px #DBCA11";
+        return "0px 0px 5px #6D38E0";
+    }
+    return (
+        <motion.div style={{fontSize: 30,
+            borderRadius: "100px",
+            marginLeft: "160px",
+            width: "80px",
+            height: "80px",
+            position: "absolute",
+            backgroundColor: "white"}} animate={{ boxShadow: addShadow(hitQuality)}}
+                    transition={{ duration: 0.5 }}>
+            <HitQualityAnimation />
+        </motion.div>
+    );
+};
+
+export default HitReference;
--- src/gameLogic/index.tsx
+++ src/gameLogic/index.tsx
@@ -9,6 +9,7 @@ import { motion } from "framer-motion";
 import {useAnimationStore} from "./Stores/animationStore.tsx";
 import {HitQuality} from "../../lib/types.ts";
 import HitQualityAnimation from "./HitQualityAnimation.tsx";
+import HitReference from "./HitReference.tsx";
 
 const Index = () => {
     const audioRef = useRef<Tone.Player | null>(null);
@@ -20,13 +21,7 @@ const Index = () => {
         if(shadow)
             setTimeout(() => disableShadow(), 100);
     }, [shadow])
-    const addShadow = (hitQuality) => {
-        if(shadow && hitQuality === HitQuality.Perfect) 
-            return "0px 0px 50px 10px #50DD49";
-        else if(shadow && hitQuality === HitQuality.Good)
-            return "0px 0px 50px 10px #DBCA11";
-        return "0px 0px 5px #6D38E0";
-    }
+    
     useEffect(() => {
         if(jump)
             setTimeout(() => {
@@ -41,16 +36,7 @@ const Index = () => {
     return (
         <div>
             <button onClick={() => startSong(audioRef)} style={{zIndex: -1}}>start</button>
-            <motion.div style={{fontSize: 30,
-                borderRadius: "100px", 
-                marginLeft: "160px", 
-                width: "80px", 
-                height: "80px", 
-                position: "absolute", 
-                backgroundColor: "white"}} animate={{ boxShadow: addShadow(hitQuality)}}
-                        transition={{ duration: 0.5 }}>
-                <HitQualityAnimation />
-            </motion.div>
+            <HitReference />
             
             <div style={{display: "flex", flexDirection: "row"}}>
               <IncomingHits />
