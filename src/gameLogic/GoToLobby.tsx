1aff876de1ed6fe011e0e836f8a1de814b81a67c
teooko <teobogdann@yahoo.com>
2024-12-03 19:12:20 +0200

    Connect to web socket when navigating to lobby with id

--- src/gameLogic/GoToLobby.tsx
+++ src/gameLogic/GoToLobby.tsx
@@ -1,9 +1,14 @@
 ﻿import GameLogic from "./index.tsx";
 import {useParams} from "react-router";
 import {useEffect} from "react";
-
+import useWebSocket from 'react-use-websocket';
 const GoToLobby = () => {
     const { lobbyId } = useParams();
+    const socketUrl = 'ws://localhost:5192/hubs/lobby';
+    
+    const {sendMessage} = useWebSocket(socketUrl, {
+        onOpen: () => sendMessage("{\"protocol\": \"json\", \"version\": 1}")
+    })
     useEffect(() => console.log(lobbyId), []);
     return (
         <>
