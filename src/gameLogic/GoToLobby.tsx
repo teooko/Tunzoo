63f782fa50bd84ed8279f51e08da5d55551dbb00
teooko <teobogdann@yahoo.com>
2024-12-04 11:56:47 +0200

    Use signalr client instead of web socket hook

--- src/gameLogic/GoToLobby.tsx
+++ src/gameLogic/GoToLobby.tsx
@@ -1,17 +1,12 @@
 ﻿import GameLogic from "./index.tsx";
 import {useParams} from "react-router";
-import {useEffect} from "react";
-import useWebSocket from 'react-use-websocket';
+import SignalRComponent from "./SingalRComponent.tsx";
 const GoToLobby = () => {
     const { lobbyId } = useParams();
-    const socketUrl = 'ws://localhost:5192/hubs/lobby';
-    
-    const {sendMessage} = useWebSocket(socketUrl, {
-        onOpen: () => sendMessage("{\"protocol\": \"json\", \"version\": 1}")
-    })
-    useEffect(() => console.log(lobbyId), []);
+
     return (
         <>
+            <SignalRComponent />
             <GameLogic />
         </>
     );
---
+++ src/gameLogic/SingalRComponent.tsx
@@ -0,0 +1,38 @@
+﻿import React, { useEffect } from "react";
+import * as signalR from "@microsoft/signalr";
+
+const SignalRComponent = () => {
+    useEffect(() => {
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl("ws://localhost:5192/hubs/lobby", {
+                skipNegotiation: true,
+                transport: signalR.HttpTransportType.WebSockets,
+            })
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
+
+        const start = async () => {
+            try {
+                await connection.start();
+                console.log("SignalR Connected.");
+            } catch (err) {
+                console.log(err);
+                setTimeout(start, 5000);
+            }
+        };
+
+        connection.onclose(async () => {
+            await start();
+        });
+
+        start();
+
+        return () => {
+            connection.stop();
+        };
+    }, []);
+
+    return <div>SignalR Connection</div>;
+};
+
+export default SignalRComponent;
