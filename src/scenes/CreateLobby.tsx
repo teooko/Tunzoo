e81c10bc0e0201bc764920919617398000fa672a
teooko <teobogdann@yahoo.com>
2024-12-10 21:07:09 +0200

    Automatically connect to created lobby

--- src/scenes/CreateLobby.tsx
+++ src/scenes/CreateLobby.tsx
@@ -1,12 +1,13 @@
 ﻿import {useEffect} from "react";
 import axios from "axios";
 import {URLS} from "../../lib/constants.ts";
+import {useNavigate} from "react-router";
 
 const CreateLobby = () => {
-    
+    const navigate = useNavigate();
     useEffect(() => {
         axios.get(`${URLS.API_URL}/lobby`)
-            .then((response) => console.log(response.data))
+            .then((response) => navigate(`/lobby/${response.data}`))
             .catch((error) => console.error('Error:', error));
     }, []);
     
